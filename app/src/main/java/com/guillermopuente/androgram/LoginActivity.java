5d57e0d95f9f7a24d150575f0f74b06dd63fbd95
Guillermo Puente <guillermo.ps09@gmail.com>
2017-03-25 17:52:13 -0300

    Created and implemented register activity

--- app/src/main/java/com/guillermopuente/androgram/LoginActivity.java
+++ app/src/main/java/com/guillermopuente/androgram/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.guillermopuente.androgram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.guillermopuente.androgram.view.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/guillermopuente/androgram/view/RegisterActivity.java
+++ app/src/main/java/com/guillermopuente/androgram/view/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.guillermopuente.androgram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.guillermopuente.androgram.R;
 
@@ -11,5 +12,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        showToolbar(getResources().getString(R.string.toolbar_title_createaccount), true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
